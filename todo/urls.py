a20ad1cbf5211403cabfc8936d408d3c5b60bc43
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 19:44:50 +0000

    Expose logout API endpoint

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('tasks/', views.task_list, name='task_list'),
-    path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("tasks/", views.task_list, name="task_list"),
+    path("tasks/<int:pk>/", views.task_detail, name="task_detail"),
 ]
-
