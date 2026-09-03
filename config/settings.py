c86a1686afe4a71280114976bcfa34a18cb180e2
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 19:44:40 +0000

    Add API request throttling

--- config/settings.py
+++ config/settings.py
@@ -84,6 +84,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/minute",
+        "user": "120/minute",
+    },
 }
 
 LANGUAGE_CODE = "en-us"
