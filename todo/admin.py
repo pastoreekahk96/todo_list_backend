093f54927ca9fc38fbfc2965c4be5d5cf04c63e4
Pastoreekahk S Arthur <139224166+pastoreekahk96@users.noreply.github.com>
2026-09-03 19:23:21 +0000

    chore: improve Django admin for tasks

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "completed", "due_date", "created_at")
+    list_filter = ("completed", "due_date")
+    search_fields = ("title", "description", "user__username")
+    ordering = ("-created_at",)
